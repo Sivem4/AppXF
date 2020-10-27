f3dddd829220b696fbc2b53df63a75dcce2017d5
Mateusz Siewierski <siewier1@gmail.com>
2020-10-27 15:57:31 +0100

    4

---
+++ AppXF/AppXF.iOS/ExtendedTabbedPageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using TabbedPageDemo.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(ExtendedTabbedPageRenderer))]
+namespace TabbedPageDemo.iOS
+{
+	public class ExtendedTabbedPageRenderer : TabbedRenderer
+	{
+		public override void ViewWillLayoutSubviews()
+		{
+			UITextAttributes normalTextAttributes = new UITextAttributes();
+			normalTextAttributes.Font = UIFont.FromName("Arial", 18.0F);
+
+			foreach (UIViewController vc in ViewControllers)
+			{
+				//Adjust the title's position   
+				vc.TabBarItem.TitlePositionAdjustment = new UIOffset(0, -14);
+				vc.TabBarItem.SetTitleTextAttributes(normalTextAttributes, UIControlState.Normal);
+
+				//Adjust the icon's position
+				//vc.TabBarItem.ImageInsets = new UIEdgeInsets(-36, 0, 36, 0);
+			}
+		}
+	}
+}
